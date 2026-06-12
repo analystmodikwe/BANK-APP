ea19505d51a48df78be5216c8c8f16ef5077a4e0
lesedi <analystmodikwe18@gmail.com>
2026-06-12 02:58:32 +0200

    added event listeners on withdraw and interset button

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const ownerBalance = document.getElementById("balance");
 const ownerInput = document.getElementById("amount-input");
 const depositButton = document.getElementById("deposit-btn");
 const withdrawButton = document.getElementById("withdraw-btn");
-const interestButton = document.getElementById("withdraw-btn");
+const interestButton = document.getElementById("interest-btn");
 const historyList = document.getElementById("history-list");
 
 // creating a function to update the screen
@@ -88,3 +88,16 @@ depositButton.addEventListener("click", () =>{
   ownerInput.value ="";
 });
 
+// adding event listeners to withdraw button
+withdrawButton.addEventListener("click", () =>{
+  const amount = Number(ownerInput.value);
+  myAccount.withdraw(amount);
+  updateDisplay();
+  ownerInput.value ="";
+});
+
+// adding event listners to the interest button
+interestButton.addEventListener("click", () =>{
+  myAccount.addInterest();
+  updateDisplay()
+});
